704f0524fc8ceb8c357bf3a6185d7a3ede89bb68
sanjiheart <kaungzai@gmail.com>
2022-07-08 16:56:47 +0800

    Rename project, downgrade to Vue.js 2 & add README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# 與thymeleaf branch主要差異
+
+- Cookie **CSRF token**的HttpOnly flag為false，JavaScript可直接存取
+- **較不安全!!!**
--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,9 +1,6 @@
-const { createApp } = Vue
-
-createApp({
-    data() {
-        return {
-            token: document.cookie.replace(/(?:(?:^|.*;\s*)XSRF-TOKEN\s*\=\s*([^;]*).*$)|^.*$/, '$1')
-        }
+new Vue({
+    el: '#app',
+    data: {
+        token: document.cookie.replace(/(?:(?:^|.*;\s*)XSRF-TOKEN\s*\=\s*([^;]*).*$)|^.*$/, '$1')
     }
-}).mount('#app')
+});
