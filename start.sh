e726c2a74614f8b57a3f6e4069dd807db1da13ac
sanjiheart <kaungzai@gmail.com>
2022-12-09 16:46:10 +0800

    Add Gradle Wrapper & update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
-gulp build && gradle clean build && java -jar build/libs/spring-boot-form-login-1.0.0.jar
+#!/bin/bash
+
+yarn install
+gulp build
+./gradlew clean build
+java -jar build/libs/spring-boot-form-login-1.0.0.jar
